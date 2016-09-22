3970708f94f47696425d86d304290a208b667abe
gwdgitnom <gwd163nom@163.com>
2016-09-22 19:01:43 +0800

    turn a project to a new form by eclipse tool

--- src/BinAndCrop.py
+++ src/BinAndCrop.py
@@ -12,11 +12,22 @@ src = "./src"
 """
 
 
-def subfilesName(path):
+def subfilesName(directory):
     fl = []
+    subdir=os.listdir(directory)
+    """
     for i in os.walk(path, False):
         for f in i[2]:
             fl.append(f)
+    """
+    for i in subdir:
+        path=os.path.join(directory,i)
+        print(path)
+        if os.path.isdir(path):
+            fl.extend(subfilesName(path))
+        elif os.path.isfile(path):
+            fl.append(path)
+            print("add"+path)    
     return fl
 
 
@@ -62,22 +73,26 @@ def bin(fileName):
 def binaryzationJpg(src):
 
     print("start binaryzationJpg()")
-    files = subfilesName(src)
+    files = subfilesName(src)    
     length = len(files)
     print("###########", src, "##", length, "###########")
     i = 1
     for f in files:
-        if os.path.isfile(os.path.join(src , f)):
-            bin(os.path.join(src , f))
+        print("this is"+f)
+        if os.path.isfile(f):
+            #bin(os.path.join(src , f))
+            bin(f)
             i += 1
-            cropImage(os.path.join(src , f))
-
+            #cropImage(os.path.join(src , f))
+            cropImage(f)
+        print(f)
+    print("end binaryzationJpg()")
 
 """
     only process one directory.
 """
 def binaryzations():
-    binaryzationJpg("./train/test")
+    binaryzationJpg("./train/run")
 
 
 # command=raw_input
--- src/createDataSet.py
+++ src/createDataSet.py
@@ -14,6 +14,7 @@ import numpy as np
 import os
 #import theano
 import xml.etree.ElementTree
+from conda.config import subdir
 class pcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -33,23 +34,27 @@ class pcolors:
         self.ENDC = ''
 
 
-def dir_to_dataset(glob_files):
-    print("Gonna process:\n\t %s"%glob_files)
+def dir_to_dataset(path):
+    print("Gonna process:\n\t %s"%path)
     dataset = []
-    for file_count, file_name in enumerate( sorted(glob(glob_files),key=len) ):
-        image = Image.open(file_name)
-        print(file_name)
-        img = Image.open(file_name).convert('LA') #tograyscale
-        pixels = [f[0] for f in list(img.getdata())]
-        filename=os.path.basename(file_name)
-        #print(filename[0:-4])
-        #pixels.append(int(filename[0:-4]))
-        pixels.insert(0,(filename[0:-4]))
-        dataset.append(pixels)
-        if file_count % 1000 == 0:
-            print("\t %s files processed"%file_count)
-  #  print(dataset[0])
-    return np.array(dataset)
+    #for file_count, file_name in enumerate( sorted(glob(glob_files),key=len) ):
+    subdir=os.listdir(path)
+    for dir in subdir:
+        if os.path.isdir(os.path.join(path,dir)):
+            print("start read director "+os.path.join(path,dir))
+            dataset.extend(dir_to_dataset(os.path.join(path,dir))) 
+        elif os.path.isfile(os.path.join(path,dir)):
+                filename=os.path.join(path,dir)      
+                print("read file:"+filename)
+                #image = Image.open(dir)
+                img = Image.open(filename).convert('LA') #tograyscale
+                pixels = [f[0] for f in list(img.getdata())]
+                filename=os.path.basename(dir)
+                #print(filename[0:-4])
+                #pixels.append(int(filename[0:-4]))
+                pixels.insert(0,(filename[0:-4]))
+                dataset.append(pixels)
+    return dataset
 """
 for this method update at 20160913 to add new code to support to create image.xml
 http://www.cnblogs.com/wangshide/archive/2011/10/29/2228936.html
@@ -57,17 +62,16 @@ http://www.cnblogs.com/xiaowuyi/archive/2012/10/17/2727912.html
 http://www.cnblogs.com/coser/archive/2012/01/10/2318298.html
 """
 def initDataSet():
-    path="./train/test/*";
+    #path="D:\Projects\\Python\\deeplay\\src\\train\\run\\*";
+    path="./train/test"
     Data= dir_to_dataset(path)
+    Data=np.array(Data)
     # Data and labels are read
     #set a filename
     afileName = str(os.getcwd()) + "\\kmeans.data"
     #create the file
     file = open(afileName, 'w')
-
-
     #put the data into a file.
-
     print(len(Data))
     xmlFile=str(os.getcwd())+"\\image1.xml"
 
